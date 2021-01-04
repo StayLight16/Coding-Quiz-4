97dfc9bb01d87a555a2acb8112635f818acba727
Jeremy Yon <jeremyyon87@gmail.com>
2021-01-04 15:41:54 -0600

    more JS stuff

--- quizquestions.js
+++ quizquestions.js
@@ -42,7 +42,52 @@ var questionsDiv = document.querySelector("#questionsDiv");
 var timer = document.querySelector("#startTime");
 var currentTime = document.querySelector("#currentTime");
 
+// Seconds left is 15 seconds per question:
+var secondsLeft = 76;
+// Variable holds interval time
+var holdInterval = 0;
+// Variable holds penalty time
+var penalty = 10;
+// Creates new element
+var ulCreate = document.createElement("ul");
 
+//Timer starts when button hit & displays to user on screen
+timer.addEventListener("click", function () {
+    // We are checking zero because its originally set to zero
+    if (holdInterval === 0) {
+        holdInterval = setInterval(function () {
+            secondsLeft--;
+            currentTime.textContent = "Time: " + secondsLeft;
 
+            if (secondsLeft <= 0) {
+                clearInterval(holdInterval);
+                allDone();
+                currentTime.textContent = "Time's up, fam!";
+            }
+        }, 1000);
+    }
+    render(questionIndex);
+});
 
+// Show the questions & choices on page: 
+function render(questionIndex) {
+    // Clears existing data 
+    questionsDiv.innerHTML = "";
+    ulCreate.innerHTML = "";
+    // For loops to loop through all info in array
+    for (var i = 0; i < questions.length; i++) {
+        // Appends question title only
+        var userQuestion = questions[questionIndex].title;
+        var userChoices = questions[questionIndex].choices;
+        questionsDiv.textContent = userQuestion;
+    }
+    // New for each for question choices
+    userChoices.forEach(function (newItem) {
+        var listItem = document.createElement("li");
+        listItem.textContent = newItem;
+        questionsDiv.appendChild(ulCreate);
+        ulCreate.appendChild(listItem);
+        listItem.addEventListener("click", (compare));
+    })
+}
 
